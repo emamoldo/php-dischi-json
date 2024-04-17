338e859be966f6950d03a9cf5906aaacae89e381
emamoldo <moldovanemanuel92.em@gmail.com>
2024-04-17 19:58:49 +0200

    Add the logo Fix the structure of the folders

--- api.php
+++ api.php
@@ -1,13 +1,9 @@
-<?php  
-    $disk_container = file_get_contents('disks.json');
+<?php
+$disk_container = file_get_contents('disks.json');
 
-    $disk_container = json_decode($disk_container, true);
+$disk_container = json_decode($disk_container, true);
 
-    foreach ($disk_container as $key => $disc) {
-        $disk_container[$key]['ISBN'] = 'BGFD' . rand();
-    }
+header('Content-type: application/json');
 
-    header('Content-type: application/json');
-    
-    echo json_encode($disk_container);
+echo json_encode($disk_container);
 ?>
--- index.php
+++ index.php
@@ -1,43 +1,53 @@
-<?php 
+<?php
 
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Music Disks</title>
-    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'
+        integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
+
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
+
 <body>
-    
-<div id="app">
-    <ul>
-        <li v-for="(disk, index) in disks">
-        <span>
-            <img src="{{ disk.poster }}" alt="">
-            {{ disk.title }}
-            {{ disk.author }}
-            {{ disk.year }}
-            {{ disk.genre }}
-        </span>
-        </li>
-    </ul>
-</div>
+
+    <div id="app">
+        <!-- Header with the logo -->
+        <div class="header">
+            <div class="container">
+                <img class="logo" src="./assets/img/Spotify_Logo_CMYK_Black.png" alt="">
+            </div>
+        </div>
 
 
 
 
-<!-- CDN for Vue Js and Axios -->
-<script src='https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js' integrity='sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==' crossorigin='anonymous' referrerpolicy='no-referrer'></script>
+        <!-- main with the posters and the info of the disks -->
+        <div class="main">
 
-<script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+            <!-- Structure for the cards -->
+        </div>
 
-<script src='app.js'></script>
+
+        <!-- CDN for Vue Js and Axios -->
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js'
+            integrity='sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg=='
+            crossorigin='anonymous' referrerpolicy='no-referrer'></script>
+
+        <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+
+        <script src='./assets/js/app.js'></script>
 </body>
+
 </html>
 
 </body>
+
 </html>
